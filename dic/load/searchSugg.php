1444e6efe1a7ce3f67073d2f54d0d93ce4b7a1b3
Elder God Ma <pantheon@manyisles.ch>
2022-04-28 15:49:44 +0200

    quick optimisation

--- dic/load/searchSugg.php
+++ dic/load/searchSugg.php
@@ -7,7 +7,7 @@ if (!isset($_GET["dics"]) OR !isset($_GET["dicl"])){echo "Error: Insufficient In
 $search = $dic->purify($_GET["dics"], "dicWord"); $language = $dic->purify($_GET["dicl"], "number");
 
 $array = array();
-$query = "SELECT id, word FROM words WHERE word LIKE '%$search%'";
+$query = "SELECT id, word FROM words WHERE simpleWord LIKE '%$search%'";
 if ($language != 0){$query.= " AND lang = $language ";}
 $query .= " LIMIT 22";
 if ($result = $dic->dicconn->query($query)) {
