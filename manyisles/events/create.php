e29076639432a868b820d7df9eb833eeaff0a761
Elder God Ma <pantheon@manyisles.ch>
2022-12-10 11:56:22 +0100

    style, create form

--- manyisles/events/create.php
+++ manyisles/events/create.php
@@ -2,6 +2,7 @@
 require_once("g/eventEngine.php");
 $eventE = new eventEngine;
 
+
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +13,34 @@ $eventE = new eventEngine;
       echo $eventE->giveHead();
      ?>
     <style>
+    .Box {
+      border: var(--e-defaultborder);
+      border-radius: 6px;
+      margin: 0;
+      padding: 8px 10px;
+    }
+    .Box.default {
+      margin: 15px 0;
+    }
+    .Box h2, .Box h3 {
+      margin: 20px 0 10px;
+    }
+    input, textarea, select {
+      border: var(--e-defaultborder);
+      background-color: var(--e-inset);
+      font-size: var(--all-fonts-base);
+      font-family: var(--all-fonts-text);
+      padding: 5px 12px;
+      border-radius: 6px;
+    }
+    label {
+      font-size: var(--all-fonts-med);
+      font-family: var(--all-fonts-text);
+    }
+    .inBoxForm input, .inBoxForm textarea, .inBoxForm select {
+      margin: 4px 0 8px;
+      resize: none;
+    }
     </style>
 </head>
 <body>
@@ -23,14 +52,27 @@ $eventE = new eventEngine;
       ?>
       <section class="column-main">
         <h1>Create Event</h1>
-        <p>later: Series here (button to select parent series where helper/owner, none, or "create new")</p>
-        <div>
+        <div class="Box default">
+          <p>later: Series here (button to select parent series where helper/owner, none, or "create new")</p>
+        </div>
+        <div class="Box default">
           <form>
-            <input type="text" placeholder="Event Name" />
-            <input type="text" placeholder="Date and Time" />
-            <input type="text" placeholder="Location" />
-            <input type="text" placeholder="Description" />
-            <p>settings (repeats weekly, participant cap)</p>
+            <div class="inBoxForm">
+              <label for="name">Event Name</label>
+              <input type="text" name="name" id="name" placeholder="" />
+              <label for="date-happens">Date</label>
+              <input type="date" name="date-happens" id ="date-happens" value="" min="" max="" onload="fixDate(this, min)">
+              <label for="location">Location</label>
+              <input name="location" id="location" type="text" placeholder="" />
+              <label for="desc">Description</label>
+              <textarea name="desc" id="desc" placeholder="" rows="5"></textarea>
+              <h3>Extra Information</h3>
+              <label for="eType">Event Type</label>
+              <input name="type" id="eType" type="text" placeholder="RPG" />
+              <label for="date-opens">Opening Date</label>
+              <input type="date" name="date-opens" id="date-opens" value="" min="" max="" onload="fixDate(this, min)">
+              <h3>Settings</h3>
+            </div>
           </form>
         </div>
       </section>
