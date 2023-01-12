e29f35e409ff9bd19faa655a4093b2ad5b952327
Elder God Ma <pantheon@manyisles.ch>
2023-01-12 19:29:04 +0100

    Created basic parser (parser2.php)

---
+++ manyisles/Server-Side/datastructures/stack.php
@@ -0,0 +1,57 @@
+<?php
+class Stack {
+  public $top;
+  public $stack = array();
+
+  function __construct() {
+    $this->top = -1;
+  }
+
+  // check whether the stack is empty or not
+  public function isEmpty() {
+    if($this->top == -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  //create a function to return size of the stack
+  public function size() {
+     return $this->top+1;
+  }
+
+  //create a function to add new element
+  public function push($x) {
+    $this->stack[++$this->top] = $x;
+    return true;
+  }
+
+  //create a function to delete top element
+  public function pop() {
+    if($this->top >= 0){
+      //echo "<br>popped, returning ";
+      //print_r($this->stack[$this->top]);
+
+      return $this->stack[$this->top--];
+    }
+    return false;
+  }
+
+  public function top() {
+    if($this->top < 0) {
+      return false;
+    } else {
+      return $this->stack[$this->top];
+    }
+  }
+
+  function echoStack() {
+    $text = "";
+    foreach ($this->stack as $element){
+      $text .= " ".$element["type"];
+    }
+  }
+}
+
+?>
---
+++ manyisles/Server-Side/parser2.php
@@ -0,0 +1,357 @@
+<html>
+<head>
+<style>
+  p {
+    padding-left: 22px;
+  }
+
+</style>
+</head>
+<body>
+<?php
+//header('Content-Type: text/example');
+if (!class_exists("parser")){
+  require_once($_SERVER['DOCUMENT_ROOT']."/Server-Side/allBase.php");
+  require_once($_SERVER['DOCUMENT_ROOT']."/Server-Side/datastructures/stack.php");
+
+  class parser {
+    use allBase;
+    public $divTypes = [
+      "gallery" => [
+        "class" => "gallery"
+      ],
+      "wide" => [
+        "class" => "wide"
+      ],
+      "quote" => [
+        "class" => "quote",
+        "classOptions" => ["note", "left"]
+      ],
+      "highlighted" => [
+        "class" => "quote highlited",
+        "classOptions" => ["note", "left"]
+      ],
+      "code" => [
+        "class" => "code",
+        "el_type" => "span",
+        "innerType" => "line",
+        "classOptions" => ["block"]
+      ]
+    ];
+    private $safeMode = true;
+
+    function __construct($parseClear = false) {
+      if ($parseClear) {$this->safeMode = false;}
+    }
+
+    function parse($body, $extent = 0, $callback = null) {
+      //extent: -1 pure markdown, 0 markdown and special characters, 1 full html (with images&divs)
+
+      $bodyArr = explode(PHP_EOL, $body);
+      $bodyObj = new textObject();
+
+      foreach ($bodyArr as $i => $line){
+        //check line markers and define line type
+        $lineType = "";
+        if (preg_match_all("/^#+/", $line, $lineMatches)){ //header
+          $lineType = ("h" . strlen($lineMatches[0][0]));
+          $line = preg_replace("/^#+ */", "", $line);
+        }
+        else if (preg_match("/^\-/", $line)){ //ul
+          $lineType = ("li");
+          $line = preg_replace("/^\- */", "", $line);
+        }
+        $bodyObj->newline($line, $lineType);
+        if ($this->safeMode){$bodyObj->linesafe();}
+        $bodyObj->linemarkdown();
+        $bodyObj->endline();
+      }
+
+      return $bodyObj->plainBody();
+
+      // //pre-Parsedown processing
+      // if ($extent > 0) {
+      //   //images
+      //   $this->insertArray = [];$lineMatches = [];
+      //   if (preg_match_all("/^.*{([a-z]+\[.*\]).*}.*$/m", $body, $lineMatches) != false){$body = $this->parseImg($body, $this->keyTracker, $lineMatches[0]);}
+      // }
+      //
+      // if ($extent == 0){
+      //   $body = $this->Parsedown->line($body);
+      // }
+      // else {
+      //   $body = $this->Parsedown->text($body);
+      // }
+      //
+      // //post-Parsedown processing
+      // if (gettype($callback)=="object") {
+      //   $callback($body);
+      // }
+      // if ($extent > 0){
+      //   //images
+      //   if ($this->insertArray != null){
+      //     foreach ($this->insertArray as $key => $value) {
+      //       $body = substr_replace($body, $value, strpos($body, "%key".$key."key%"), 0);
+      //       $body = str_replace("%key".$key."key%", "", $body);
+      //     }
+      //   }
+      //
+      //   //all kinds of divs
+      //   $lineMatchesD = [];
+      //   if (preg_match_all("/\[(([A-Za-z]+)[A-Za-z ]*)\]/", $body, $lineMatchesD, PREG_OFFSET_CAPTURE) != false){$body = $this->parseDivs($body, $lineMatchesD);}
+      // }
+      //
+      // $specCharLvl = 2;
+      // if ($extent < 0){$specCharLvl = 0;}
+      // $body = $this->placeSpecChar($body, $specCharLvl);
+      // //$body = utf8_decode($body);
+
+    }
+
+    function parseDivs($body, $lineMatches){
+      $insertArray = [];
+      for ($i = 0; $i < count($lineMatches[0]); $i++){
+        $blockStart = $lineMatches[0][$i];
+        $startMarker  = $blockStart[0];
+        $startMarkerPos = $blockStart[1] + strlen($startMarker);
+        $fullClass = $lineMatches[1][$i][0];
+        $class = $lineMatches[2][$i][0];
+        $endMarker = "[/".$class."]";
+
+        $inBlock = substr($body, $startMarkerPos);
+        if (($endMarkerPos = strpos($inBlock, $endMarker)) === false){continue;}//no end marker found
+        $inBlock = substr($inBlock, 0, $endMarkerPos);
+
+        if (!($officialClass = $this->checkDivClass($fullClass))){continue;}
+        if ($officialClass[2] == "text"){$parsedInBlock = $this->Parsedown->text($inBlock);}
+        else {$parsedInBlock = $this->Parsedown->line($inBlock);}
+
+        $parsedBlock = "<".$officialClass[0]." class='".$officialClass[1]."'>".$parsedInBlock."</".$officialClass[0].">";
+        $insertArray[$i] = [$startMarker.$inBlock.$endMarker, $parsedBlock];
+      }
+      foreach ($insertArray as $element){
+        $body = str_replace($element[0], $element[1], $body);
+      }
+      return $body;
+    }
+    function checkDivClass($fullClass){
+      $officialClass = [0 => "div", 1 => "", 2 => "text"];
+      $classes = explode(" ", $fullClass);
+      if (!isset($this->divTypes[$classes[0]])){ return false; }
+      $officialClass[1] = $this->divTypes[$classes[0]]["class"];
+      if (isset($this->divTypes[$classes[0]]["classOptions"])){
+        foreach ($classes as $class){
+          if (in_array($class, $this->divTypes[$classes[0]]["classOptions"])){
+            $officialClass[1] .= " ".$class;
+          }
+        }
+      }
+      if (isset($this->divTypes[$classes[0]]["el_type"])){
+        $officialClass[0] = $this->divTypes[$classes[0]]["el_type"];
+      }
+      if (isset($this->divTypes[$classes[0]]["innerType"])){
+        $officialClass[2] = $this->divTypes[$classes[0]]["innerType"];
+      }
+      return $officialClass;
+    }
+
+    function parseImg($body, &$keyTracker, $lineMatches) {
+        foreach($lineMatches as $line){
+            $artId = substr($line, strpos($line, "{")+1);
+            $stringDico = substr($artId, 0, strpos($artId, "}") - 1);
+            $chunks = array_chunk(preg_split('/(\[|\])/', $stringDico), 2);
+
+            if (count(array_column($chunks, 0)) > 0 AND count(array_column($chunks, 0)) == count(array_column($chunks, 1))){
+                $img = array_combine(array_column($chunks, 0), array_column($chunks, 1));
+            }
+            else {$img = [];}
+
+            if (!isset($img["class"])){$img["class"] = "sideimg";}
+            else if ($img["class"]=="landscape"){$img["class"]="sideimg landscape";}
+            if (!isset($img["caption"])){$img["caption"] = "";}
+            if (!isset($img["src"])){$img["src"] = "";}
+            if (!isset($img["style"])){$img["style"] = "";}
+            $this->purify($img["class"]);$this->purify($img["caption"], "cleanText");$this->purify($img["src"], "cleanText");$this->purify($img["style"], "cleanText");
+            $caption = $this->parse($this->placeSpecChar($img["caption"], 0), 0);
+
+            $echoImg = '<div class="'.$img["class"].'" style="'.$img["style"].'"><a href="'.$img["src"].'" target="_blank"><img src="'.$img["src"].'" /></a><p>'.$caption.'</p></div>';
+
+            $this->insertArray[$keyTracker] = $echoImg;
+            $pos = strpos($body, "{");
+            $body = str_replace($line, "%key".$keyTracker."key%", $body);
+            $keyTracker++;
+        }
+
+        if (preg_match("/^{.*}$/", $body)){return $this->parseImg($body, $keyTracker);}
+        else {return $body;}
+    }
+  }
+
+  class textObject {
+    private $boxTypes = [
+      "paragraph" => [
+        "defaultHTMLlabel" => "p"
+      ],
+      "ul" => [
+        "defaultHTMLlabel" => "ul"
+      ]
+    ];
+    private $lineTypes = [
+      "none" => ["prefix" => "", "suffix" => ""],
+      "n" => ["prefix" => "", "suffix" => ""],
+      "h1" => ["prefix" => "<h1>", "suffix" => "</h1>"],
+      "h2" => ["prefix" => "<h2>", "suffix" => "</h2>"],
+      "h3" => ["prefix" => "<h3>", "suffix" => "</h3>"],
+      "h4" => ["prefix" => "<h4>", "suffix" => "</h4>"],
+      "h5" => ["prefix" => "<h5>", "suffix" => "</h5>"],
+      "li" => ["prefix" => "<li>", "suffix" => "</li>"],
+    ];
+
+    public $boxes;
+    public $currLine = [];
+
+    function __construct() {
+      $this->boxes = new Stack();
+      $this->boxes->push(["type"=>"base", "text" => []]);
+    }
+
+    //lines
+    function newline(string $text, string $lineType = "n") {
+      if (!isset($this->lineTypes[$lineType])){
+        $lineType = "n";
+      }
+
+      //check whether current box needs to be closed
+      while ($this->boxes->top > 0){
+        if ($this->boxes->top()["type"] == "paragraph" && $lineType != "n"){
+          $this->closeBox();
+        }
+        else if ($this->boxes->top()["type"] == "ul" && $lineType != "li"){
+          $this->closeBox();
+        }
+        else {
+          break;
+        }
+      }
+
+      //check that current box is compatible with lineType
+      if (preg_match("/h[1-5]$/", $lineType)){ //header
+        while ($this->boxes->top()["type"] != "base" && !$this->boxes->isEmpty()){
+          $this->closeBox();
+        }
+      }
+      else if ($lineType == "li"){ //list
+        if ($this->boxes->top()["type"] != "ul"){
+          $this->openBox("ul");
+        }
+      }
+      else if ($text == ""){//empty line
+        $lineType = "none";
+        if ($this->boxes->top()["type"] == "paragraph"){
+          $this->closeBox();
+        }
+      }
+      else { //new line
+        if ($this->boxes->top()["type"] != "paragraph"){
+          $this->openBox("paragraph");
+        }
+      }
+
+      $this->currLine = ["text" => $text, "type" => $lineType];
+    }
+    function linesafe(){ //make sure no user-inputted HTML is interpreted (safe mode)
+      $this->currLine["text"] = htmlspecialchars($this->currLine["text"], ENT_NOQUOTES, 'UTF-8');
+    }
+    function linemarkdown() {
+      $line = $this->currLine["text"];
+
+      //***, **, *, links
+
+      $this->currLine["text"] = $line;
+    }
+    function endline(){
+      $line = $this->currLine["text"];
+      $lineInfo = $this->lineTypes[$this->currLine["type"]];
+      $box = $this->boxes->pop();
+
+      //special rules
+      if ($this->currLine["type"]=="n" && count($box["text"]) > 0){
+        $lineInfo["suffix"] = "<br />";
+      }
+
+      $line = $lineInfo["prefix"].$line.$lineInfo["suffix"];
+      //echo "<br>endline, appending ".$line;
+      $box["text"][] = $line;
+      $this->boxes->push($box);
+      $this->boxes->echoStack();
+    }
+    //boxes
+    function openBox(string $boxType, array $HTMLlabels = []){
+      if (!isset($this->boxTypes[$boxType])){
+        return false;
+      }
+      if (count($HTMLlabels)!=2){
+        $default = $this->boxTypes[$boxType]["defaultHTMLlabel"];
+        $HTMLlabels = ["prefix" => "<$default>", "suffix" => "</$default>"];
+      }
+
+      $box = ["type" => $boxType, "text" => [], "HTMLlabels" => $HTMLlabels];
+      $this->boxes->push($box);
+    }
+    function closeBox() {
+      $box = $this->boxes->pop();
+      $parentBox = $this->boxes->pop();
+      $parentBox["text"][] = $box["HTMLlabels"]["prefix"].implode(PHP_EOL, $box["text"]).$box["HTMLlabels"]["suffix"];
+      $this->boxes->push($parentBox);
+    }
+    //other
+    function plainBody(){
+      echo "<br><br>BEGIN RETURN<br>";
+      //print_r($this->boxes);
+      $return = "";
+      while (!$this->boxes->top == 0){
+        $this->closeBox();
+      }
+
+      $return .= implode(PHP_EOL, $this->boxes->pop()["text"]);
+      return $return;
+    }
+  }
+
+  // potentially: represent body, lines in classes instead of arrays
+  // class textObject {
+  //   private $body = [];
+  //
+  // }
+  // class lineObject {
+  //   private $type;
+  //   private $htmlLabel;
+  //   private $text;
+  //
+  // }
+}
+
+$input = <<<helle
+## I'm a title
+what's up?
+-
+list element
+-another list element
+
+
+
+
+
+the sky, lol
+###Subtitle
+more Text
+
+what happens here?
+########Looooooong loooooong tiiitle
+helle;
+
+$parser = new parser();
+echo $parser->parse($input, 0);
+
+?>
+</body>
